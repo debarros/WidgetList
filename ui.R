234eefc6ba8ef182c2c62720173b6c2b34591d7e
pj.debarros@gmail.com <pj.debarros@gmail.com>
2015-12-05 15:56:15 -0500

    Added scrollable grid tab to ui.R.

--- ui.R
+++ ui.R
@@ -15,8 +15,29 @@ shinyUI(fluidPage(
                fluidRow(
                  column(width = 6, uiOutput("NumberOfRows")),
                  column(width = 6, uiOutput("NumberOfColumns")) ),
-               fluidRow(
-                 column(width = 12, uiOutput("RadioGrid")) ) )
+               fluidRow(column(width = 12, uiOutput("RadioGrid")) ) ),
+      tabPanel(title = "Scrollable grid",
+               sidebarLayout(
+                 uiOutput("RadioGrid2"),
+                 mainPanel(
+                   fluidRow(
+                     column(width = 6, uiOutput("NumberOfRows2")),
+                     column(width = 6, uiOutput("NumberOfColumns2")) ),
+                   fluidRow(uiOutput("InlineChooser")),
+                   fluidRow(uiOutput("PositionChooser")) ) ) ),#end of tabPanel
+      tabPanel(title = "Extract from grid",
+               sidebarLayout(
+                 sidebarPanel(
+                   style = "overflow-y:scroll; max-height: 600px; position:relative;",
+                   uiOutput("RadioGrid3")),
+                 mainPanel(
+                   fluidRow(
+                     column(width = 6, uiOutput("NumberOfRows3")),
+                     column(width = 6, uiOutput("NumberOfColumns3")) ),
+                   fluidRow(uiOutput("summary"))
+                 )#end of mainPanel
+               )#end of sidebarLayout
+      )#end of tabPanel
     )#end of tabsetPanel
   )#end of mainPanel
 ))#end of shinyUI
